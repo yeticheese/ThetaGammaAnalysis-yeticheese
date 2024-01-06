c9f5800845d38e6785ff4d77b4758afc2a9c1a86
yeticheese <abumohd.zalrahman@gmail.com>
2024-01-07 03:57:27 +0600

    Fixed attributes

--- src/signal.py
+++ src/signal.py
@@ -1,8 +1,16 @@
-import numpy
+import numpy as np
 import emd.sift as sift
 class signal():
-    def __init__(self,array:np.ndarray, sampling_rate:int):
+    def __init__(self,array:np.ndarray, sample_rate:int):
         self.signal = array
-        self.duration= len(array)/sampling_rate
-
-    def iter_sift(self,sample_rate)
+        self.sample_rate = sample_rate
+        self.duration= np.linspace(0,len(array)/sample_rate,len(array))*1000
+        self.imf= None
+        self.mask_freq = None
+        
+    def iter_sift(self, mask='zc')-> tuple:
+        self.imf,self.mask_freq =sift.iterated_mask_sift(self.signal,
+                                                  mask_0=mask,
+                                                  sample_rate=self.sample_rate,
+                                                  ret_mask_freq=True)
+    
