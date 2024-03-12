7a250aed0e50de6976cf30fde3c81727744924ee
yeticheese <abumohd.zalrahman@gmail.com>
2024-03-12 15:54:48 +0600

    Added a dictionary generator to easily populate instances

--- src/Trial.py
+++ src/Trial.py
@@ -6,6 +6,37 @@ import scipy.io as sio
 import numpy as np
 import os
 
+def generate_class_dict(data_dict:dict,signal:np.ndarray,sample_rate:float,freq_range:tuple):
+    class_dict={}
+    class_dict['signal'] = signal
+    class_dict['sample_rate'] = sample_rate
+    class_dict['freq_range'] = freq_range
+    class_dict['REM']= []
+    class_dict['rem_states'] = np.empty((0,2)).astype(int)
+    for key,value in data_dict.items():
+        rem_dict={}
+        if re.search(r'REM (\d{1,2})',key):
+            rem_dict['period'] = value['start-end']
+            class_dict['rem_states'] = np.vstack([class_dict['rem_states'], rem_dict['period']])
+            rem_dict['signal'] = class_dict['signal'][rem_dict['period'][0]:rem_dict['period'][1]+1]
+            rem_dict['imf'] = value['IMFs']
+            rem_dict['mask_freq'] = value['IMF_Frequencies']
+            rem_dict['IP'] = value['Instantaneous Phases']
+            rem_dict['IF'] = value['Instantaneous Frequencies']
+            rem_dict['IA'] = value['Instantaneous Amplitudes']
+            rem_dict['cycles'] = value['Cycles']
+            rem_dict['sample_rate'] = class_dict['sample_rate']
+            rem_dict['freq_range'] = class_dict['freq_range']
+            class_dict['REM'].append(REM_Segment(**rem_dict))
+        elif key == 'header_info':
+            class_dict['rat'] = value['rat']
+            class_dict['study_day'] = value['study_day']
+            class_dict['condition'] = value['condition']
+            class_dict['trial']= value['trial']
+
+    return class_dict
+
+
 @dataclass
 class Trial(SleepSignal):
     rat: int = field(default_factory=None)
